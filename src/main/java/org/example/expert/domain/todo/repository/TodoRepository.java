5bc6ccac01921b2cbce06ce8d3d078f0906006ee
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-04-01 23:29:39 +0900

    feat: JPQL Query문 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,15 +7,40 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
+    // 1. 할일 리스트 검색
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
+    // 2. 날씨만으로 검색
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
+            "WHERE t.weather = :weather ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByWeather(@Param("weather") String weather, Pageable pageable);
+
+    // 3. 수정일 기간으로만 검색
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
+            "WHERE t.modifiedAt BETWEEN :startDate AND :endDate ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByModifiedAtBetween(
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            Pageable pageable);
+
+    // 4. 날씨와 기간 모두 검색
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
+            "WHERE t.weather = :weather " +
+            "AND t.modifiedAt BETWEEN :startDate AND :endDate ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByWeatherAndModifiedAtBetween(
+            @Param("weather") String weather,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            Pageable pageable);
+
     @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
+            "LEFT JOIN FETCH t.user " + // 단건 조회라도 해도 유저 정보를 함게 가지고 오는게 성능상 이득일 듯
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
